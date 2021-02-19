e71cd708601e6b89e009da2c46b497b8435f5d23
unknown <skafosbiz@gmail.com>
2021-02-19 18:42:53 +0200

    make it better

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 
 function Home() {
-	var imageName = require('https://skafosbiz.wixsite.com/mysite')
+	
 
   return (
     <div className="home">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
+		  <Link to='https://skafosbiz.wixsite.com/mysite'>
             <img
               class="img-fluid rounded mb-4 mb-lg-0" 
               src={imageName}
 			 
               alt=""
             />
+			</Link>
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Home</h1>
