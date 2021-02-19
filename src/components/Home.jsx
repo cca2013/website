81f0c2398877911627996443044a68f854717277
unknown <skafosbiz@gmail.com>
2021-02-19 19:14:02 +0200

    make it better

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,18 +8,21 @@ function Home() {
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
-		  <a href="http://vresto.eu">
+		  <a href="https://skafosbiz.wixsite.com/mysite">
             <img
               class="img-fluid rounded mb-4 mb-lg-0" 
               src="https://previews.123rf.com/images/liaarevadze/liaarevadze1706/liaarevadze170600126/81072644-flat-design-of-website-under-construction-web-page-building-process-site-form-layout-of-web-developm.jpg"
 			 
               alt=""
-            /></a>
+            /></a></div>
+			<div>
+			<right>
 		<a href="http://vresto.eu">
          <img  class="img-fluid rounded mb-4 mb-lg-0" 
 		 alt="K.P.Anastasiadis-vresto.eu" src="http://anastasiadiskonstantinos.appspot.com/images/cpa7.jpg"
          width="450" height="170" />
-      </a>
+			</a>
+			</right
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Home</h1>
