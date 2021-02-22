798b2acf906a4da693b45bc9742a9fc77f48aec0
unknown <skafosbiz@gmail.com>
2021-02-22 12:55:14 +0200

    make it better

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,14 +14,14 @@ function Home() {
             </p>
         
 		  <a href="https://skafosbiz.wixsite.com/mysite">
-		 
+		   <h2>WEB SITES</h2>
             <img
               class="img-fluid rounded mb-4 mb-lg-0" 
               src="https://static.wixstatic.com/media/ea71bb_370144e721bc43c289bfb35fed3376d1~mv2_d_4500_2124_s_2.jpg/v1/fill/w_1224,h_534,al_b,q_85,usm_0.66_1.00_0.01/ea71bb_370144e721bc43c289bfb35fed3376d1~mv2_d_4500_2124_s_2.webp"
 			 width="430" height="240" 
               alt=""
             /></a>
-			<p></p><p></p>
+			<p></p>
 			<a href="http://vresto.eu">
 			  <h2>BLOGS</h2>
          <img  class="img-fluid rounded mb-4 mb-lg-0" 
@@ -36,7 +36,7 @@ function Home() {
 		<h2>E-SHOPs</h2>
          <img  class="img-fluid rounded mb-4 mb-lg-0" 
 		 alt="K.P.Anastasiadis-eshops" src="https://static.wixstatic.com/media/6f5d17_cc6f156710bd4479a49aba7f435b92da~mv2.jpeg/v1/fill/w_469,h_1084,al_c,q_85/6f5d17_cc6f156710bd4479a49aba7f435b92da~mv2.webp"
-         width="300" height="60" />
+         width="300" height="50" />
 			</a>
 		 
           </div>
